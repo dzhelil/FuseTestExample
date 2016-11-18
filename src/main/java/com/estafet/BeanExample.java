73297e64bb1fbff5e807c0fbb07b1a3749f37390
Dzhelil Ramadan <dzhelilramadan@gmail.com>
2016-11-18 10:42:12 +0200

    Bean example

---
+++ src/main/java/com/estafet/BeanExample.java
@@ -0,0 +1,13 @@
+package com.estafet;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Created by DRamadan on 18-Nov-16.
+ */
+public class BeanExample extends RouteBuilder{
+    @Override
+    public void configure() throws Exception {
+        from("").to("");
+    }
+}
